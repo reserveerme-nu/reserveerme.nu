87f62ce4996b28cb234c00a722e06b34b863ef34
Maikel Schuurmans <264235@student.fontys.nl>
2020-03-18 08:28:40 +0100

    Updated basic Room design.

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -2,7 +2,7 @@
     <b-container fluid>
         <b-row>
             <b-col cols="9" id="mainDiv">
-                <b-row>
+                <b-row id="titleDiv">
                     <b-col cols="10">
                         <h1>Asimov</h1>
                     </b-col>
@@ -12,9 +12,35 @@
                 </b-row>
                 <b-row>
                     <b-col>
+                        <p>4th Floor</p>
+                        <p>4 Persons</p>
+                    </b-col>
+                </b-row>
+                <b-row>
+                    <b-col>
                         <h2 id="status">OCCUPIED</h2>
                     </b-col>
                 </b-row>
+                <b-row>
+                    <b-col>
+                        <H4>by Xander Steinmann</H4>
+                    </b-col>
+                </b-row>
+                <b-row>
+                    <b-col>
+                        <H4>until 14:45</H4>
+                    </b-col>
+                </b-row>
+                <b-row>
+                    <button type="button" style="margin: 20px auto;">FIND A FREE ROOM</button>
+                </b-row>
+                <b-row>
+                    <button type="button">END</button>
+                </b-row>
+                <b-row>
+                    <button type="button">EXTEND</button>
+                </b-row>
+                <img src="../assets/Fontys-Logo.png" height="100" width="160"  alt="Fontys Logo" id="logo"/>
             </b-col>
             <b-col cols="3" id="calenderDiv">
                 <calender></calender>
@@ -36,38 +62,65 @@
 </script>
 
 <style scoped>
+    p{
+        color: white;
+        font-family: "Open Sans", serif;
+        font-size: 22px;
+        float: left;
+        padding-right: 25px;
+    }
     h1{
         color: white;
         font-family: 'Rokkitt', serif;
         font-size: 100px;
         float: left;
-        padding-top: 10px;
-        padding-left: 10px;
     }
-    h2, h3, h4{
+    h2, h3{
         font-family: 'Rokkitt', serif;
         color: white;
     }
+    h4{
+        color: white;
+        font-family: "Open Sans", serif;
+        font-size: 50px;
+        font-weight: 300;
+    }
 
+    button{
+        color: #c12c3f;
+        font-family: 'Rokkitt', serif;
+        font-size: 30px;
+        padding-left: 15px;
+        padding-right: 15px;
+        margin-top: 20px;
+    }
+    #logo{
+        position: absolute;
+        bottom: 40px;
+        right: 60px;
+        float: right;
+    }
     #digiclock{
         color: white;
         float: right;
-        font-size: 80px;
-        padding-top: 10px;
-        padding-right: 20px;
-        font-family: "Lato Light", serif;
-        font-weight: 100;
+        font-size: 70px;
+        font-family: "Open Sans", serif;
+        font-weight: 300;
     }
     #status{
         float: none;
         font-size: 200px;
-        padding-top: 150px;
-    }
+        padding-top: 100px;
+        height: 320px;
+     }
     #titleDiv{
-        height: 120px;
+        height: 100px;
     }
     #mainDiv{
         background-color: #c12c3f;
         height: 100vh;
+        padding-left: 50px;
+        padding-right: 50px;
+        padding-top: 10px;
     }
 </style>
